37fc5071293b7e4adf9605f911b993aab9f803dd
DESKTOP-2IDV1SC\CANER <canercakman@gmail.com>
2021-11-09 14:14:40 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri kaydınız oluşturuldu : " + musteri.Ad + " " + musteri.Soyad);
+        }
+        public void Listele(Musteri musteri)
+        {
+           
+            Console.WriteLine("Müşteri listelendi  :  " + musteri.Ad + " " + musteri.Soyad);
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Kaydınız Silindi  :  " + musteri.Ad + "  " + musteri.Soyad);
+
+
+        }
+    }
+}
+
+       
+    
+        
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri Musteri1 = new Musteri();
+            Musteri1.Ad = "Caner";
+            Musteri1.Soyad = "Çakman";
+            Musteri1.Id = 35;
+
+            Musteri Musteri2 = new Musteri();
+            Musteri2.Ad = "Aytuğ";
+            Musteri2.Soyad = "Yeşilyurt";
+            Musteri2.Id = 61;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(Musteri1);
+            musteriManager.Sil(Musteri1);
+            musteriManager.Listele(Musteri1);
+
+          
+
+        }
+    }
+}
